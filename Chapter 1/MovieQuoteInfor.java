c9ca21ce6661e79bfd5e405f0f51c826223b1c5b
canogc801 <generose.cano.28@gmail.com>
2024-08-14 23:11:09 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,7 @@
+public class MovieQuoteInfor {
+   public static void main(String args[]) {
+      System.out.println("Love means never having to say you're sorry.");
+      System.out.println("Love Story");
+      System.out.println("1970");
+   }
+}
