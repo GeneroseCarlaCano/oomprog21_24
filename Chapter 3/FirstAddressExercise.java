e65b5393d1d69c3f456c3613338a3eacc03c063c
GC_Cano <generose.cano.28@gmail.com>
2024-09-20 21:53:29 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,10 @@
+ class FirstAddressExcercise {
+    public static void displayAddress() {
+        System.out.println("realme");
+        System.out.println("SM City, Cebu");
+        System.out.println("Company: realme");
+    }
+    public static void main(String[] args) {
+        displayAddress();
+    }
+}
